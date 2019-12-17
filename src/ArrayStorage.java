cd0bed3a6d54ca3166058cdf20629de184a85141
Anatoly <abrekov@ya.ru>
2019-12-18 01:56:05 +0300

    implement ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,65 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int length = size();
+        for (int i = 0; i < length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int length = size();
+        Resume resume = null;
+        for (int i = 0; i < length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int newSize = size() - 1;
+        for (int i = 0; i <= newSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, newSize - i);
+                storage[newSize] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int length = size();
+        Resume[] resumes = new Resume[length];
+        for (int i = 0; i < length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
